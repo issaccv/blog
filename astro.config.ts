922ebf137d6bca3e93f70661911d46159060ea45
issaccv <17275050+issaccv@users.noreply.github.com>
2025-12-16 19:24:26 +0800

    change site

--- astro.config.ts
+++ astro.config.ts
@@ -19,7 +19,8 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://blog.issacc.bid',
+  site: 'https://www.issacc.top',
+  // base: '/blog',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
